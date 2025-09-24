dca33cd957bc92de77538252bd2d662c11a1cd9b
SheilaChiesa <sheilamagalichiesa@gmail.com>
2025-09-23 21:51:40 -0300

    Modificaciones

--- js/main.js
+++ js/main.js
@@ -1,18 +1,58 @@
+let clientes = []
+let turnos = []
+let indice = 0
+const minimoDeTatuaje = 20000
 
+let continuar = true
+while (continuar) {
+    let menu = parseInt(prompt("Ingrese 1 para Ver turnos, 2 para consultas, 3 Cancelar turno, 4. Agenda tu turno, 5. Salir"))
+    switch (menu) {
+        case 1:
+            if (indice === 0) {
+                console.log("Turnos disponibles esta semana por la Siesta")
+            } else {
+                mostrarTurnos(clientes, turnos, indice)
+            }
+            break
+        case 2:
+            console.log("Contame que tamaño, zona y diseño te gustaria hacerte")
+            break
+        case 3:
+            console.log("Cancelaste tu turno!")
+            break
+        case 4:
+            let Nombre = prompt("Ingrese su nombre")
+            console.log("Cliente " + Nombre)
+            saludarCliente(Nombre)
 
-function saludarCliente (Nombre) {
-console.log("Bienvenido a Vudu!!" + Nombre + " Agenda tu turno!")
+            let cantidad = Number(prompt("Cuantos tatuajes queres hacerte?"))
+            if (cantidad <= 0 || isNaN(cantidad)) {
+                alert("Porfavor ingrese un numero de tatuajes por sesion valido.")
+                break
+            }
+             calcularTatuajes(cantidad)
+            turnos[indice] = cantidad
+            clientes[indice] = Nombre
+            indice++
+            break
 
+        case 5:
+            continuar = false
+            console.log("Gracias!!")
+            break
+        default:
+            alert("Opcion incorrecta")
+    }
 }
 
-function calcularTatuajes (TatuajesporSesion) {
-    const minimoDeTatuaje = 20000
-     if(TatuajesporSesion <= 0) {
-        alert("Porfavor ingrese un numero de tatuajes por sesion.")
-        return
-     }
-     let total = TatuajesporSesion * minimoDeTatuaje
-     alert("El total de tu sesion sera de: " + total)
+function saludarCliente(Nombre) {
+    console.log("Bienvenido a Vudu!!" + Nombre + " Agenda tu turno!")
+}
+
+function calcularTatuajes(TatuajesporSesion) {
+
+    let total = TatuajesporSesion * minimoDeTatuaje
+    alert("El total de tu sesion sera de: " + total)
 }
 
 function mostrarTurnos(clientes, turnos, total) {
@@ -21,65 +61,3 @@ function mostrarTurnos(clientes, turnos, total) {
         console.log(clientes[i] + " - " + turnos[i] + " tatuajes")
     }
 }
-
-
-let clientes =  []
-let turnos =  []
-let indice = 0
-
-
-let continuar = true
-while(continuar) {
-   
-
-let menu = parseInt(prompt("Ingrese 1 para Ver turnos, 2 para consultas, 3 Cancelar turno, 4 Cuantos tatuajes quieres hacerte?"))
-switch(menu) {
-    case 1:
-        if (indice === 0) {
-        console.log("Turnos disponibles esta semana por la Siesta")
-        } else {
-        mostrarTurnos(clientes, turnos, indice)
-        }
-        break
-        case 2:
-        console.log("Contame que tamaño, zona y diseño te gustaria hacerte")
-        break
-        case 3:
-         console.log("Cancelaste tu turno!")
-        break
-        case 4:
-        let Nombre = prompt("Ingrese su nombre")
-            clientes[indice] = Nombre
-            console.log("Cliente " + Nombre)
-            saludarCliente(Nombre)
-
-           let cantidad = Number(prompt("Cuantos tatuajes queres hacerte?"))
-           calcularTatuajes(cantidad)
-           turnos[indice] = cantidad
-            indice++
-           break
-           default:
-            alert("Opcion incorrecta")
-            }
-     
-
-let elegir = prompt("Desea agendar otro turno o consulta? (si/no)").toLowerCase()
-if(elegir == "no") {
-    continuar = false
-    console.log("Gracias!!")
-}
-
-
-    }
-
-    
-
-
-
-
-
-
-
-
-
-
